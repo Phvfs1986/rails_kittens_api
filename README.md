8aea8f60cb57a22c0538f78c3298468f84637bf2
Phvfs1986 <phvfs19@gmail.com>
2023-06-05 15:15:41 -0300

    work

---
+++ README.md
@@ -0,0 +1,8 @@
+# README
+
+Building a Simple Kittens API
+
+This is a fast and straightforward project to set up a Rails app to be a data-producing API… which is just a fancy way of saying that all controller methods will render data instead of HTML. This is a drill in quickly building a pure vanilla RESTful resource.
+
+Odin Project page:
+https://www.theodinproject.com/lessons/ruby-on-rails-kittens-api
---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens }
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kitten }
+    end
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:notice] = 'Kitten created!'
+      redirect_to @kitten
+    else
+      flash.now[:warning] = 'Something went wrong!'
+      render :new
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      flash[:notice] = 'Kitten updated!'
+      redirect_to @kitten
+    else
+      flash.now[:warning] = 'Something went wrong!'
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.destroy
+      flash[:notice] = 'Kitten was destroyed! =('
+      redirect_to root_path
+    else
+      flash.now[:warning] = 'Something went wrong!'
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
